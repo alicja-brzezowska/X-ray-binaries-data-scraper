cb7255b080f6d4bec17d9b471111ea62b27a8435
alicja-brzezowska <brzezowskaalicja0@gmail.com>
2025-08-21 14:30:58 +0200

    Add file

---
+++ scripts/get_UKIRT.py
@@ -0,0 +1,130 @@
+import os
+import csv
+import numpy as np
+from astropy.coordinates import SkyCoord
+import astropy.coordinates as coord
+import astropy.units as u
+from astroquery.ukidss import Ukidss
+
+
+from functions import read_csv, save_results_to_csv, rect_to_radec
+
+
+def query_UKIRT(ra_deg, dec_deg, radius_arcsec= 30.0):
+    """Query the UKIRT catalog for objects in 30'' around a specified coordinate"""
+    center = SkyCoord(ra_deg, dec_deg, unit=(u.deg, u.deg), frame='icrs')
+    radius = radius_arcsec * u.arcsec
+
+    table = Ukidss.query_region(
+        center,
+        radius=radius,
+        programme_id='GPS',
+        database='UKIDSSDR11PLUS',  # selected database
+    )
+    return table
+
+
+def find_brightest_UKIRT(table, bh_coord):
+    """
+    Given an astroquery.ukidss table and black_hole coordinates 
+    return a table of brightest objects around each with 'ra','dec','kAperMag3','kAperMag3Err', 
+    and separation columns
+    """
+    cols = ["ra", "dec", "kAperMag3", "kAperMag3Err"]
+
+    if (table is None) or (len(table) == 0):
+        print("No data found")
+        return None, None
+    
+    tab = table[cols].filled(np.nan)
+    tab.sort('kAperMag3')
+    best = tab[0]
+
+    k_coord = SkyCoord(float(best['ra']), float(best['dec']), unit='deg', frame='icrs')
+    sep_arcsec = bh_coord.separation(k_coord).arcsec
+
+    return best, sep_arcsec
+
+
+def main():
+    input_file = 'results/black_holes.csv'
+    output_file = 'results/black_holes_with_UKIRT.csv'
+    radius_arcsec = 30.0
+
+    black_holes = read_csv(input_file)
+
+    results = []
+
+    # Extract RA and DEC data from the black_holes file
+    for bh in black_holes:
+        name, bh_ra, bh_dec = bh[0], bh[1], bh[2]
+
+        # Transform black hole RA and DEC into degrees for query purposes
+        bh_coord = SkyCoord(bh_ra, bh_dec, unit=(u.hourangle, u.deg), frame='icrs')
+
+        # Query data from the UKIRT database 
+        try:
+            table = query_UKIRT(bh_coord.ra.deg, bh_coord.dec.deg, radius_arcsec=radius_arcsec)
+        except Exception:
+            table = None
+
+        if (table is None) or (len(table) == 0):
+            print(f"[INFO] No UKIDSS GPS sources found within {radius_arcsec}\" of {name}")
+            results.append({
+                'bh_name': name,
+                'bh_ra': bh_ra,
+                'bh_dec': bh_dec,
+                'star_ra': "Nothing found",
+                'star_dec': "Nothing found",
+                'sep_arcsec': "Nothing found",
+                'k_mag': "Nothing found",
+                'k_mag_err': "Nothing found"
+            })
+            continue
+
+        best, sep_arcsec = find_brightest_UKIRT(table, bh_coord)
+        if not best:
+            continue
+
+        # Convert star coordinates into sexagesimal for output
+        try:
+            star_coord = SkyCoord(float(best['ra']), float(best['dec']), unit=('deg','deg'), frame='icrs')
+            star_ra = star_coord.ra.to_string(unit=u.hour, sep=":", precision=2)   
+            star_dec = star_coord.dec.to_string(unit=u.deg, sep=":", precision=2, alwayssign=True)  
+            
+        except Exception: # If nothing is found
+            star_ra, star_dec = None, None 
+
+        # Append results
+        results.append({
+            'bh_name': name,
+            'bh_ra': bh_ra,
+            'bh_dec': bh_dec,
+            'star_ra':star_ra,
+            'star_dec': star_dec,
+            'sep_arcsec': float(sep_arcsec),
+            'k_mag': float(best['kAperMag3']) if best['kAperMag3'] is not None else None,
+            'k_mag_err': float(best['kAperMag3Err']) if best['kAperMag3Err'] is not None else None,
+        })
+
+
+    header = [
+        'bh_name',
+        'bh_ra','bh_dec',
+        'star_ra','star_dec',
+        'sep_arcsec','k_mag','k_mag_err'
+    ]
+    # Save results 
+    save_results_to_csv(results, output_file, header)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+ 
