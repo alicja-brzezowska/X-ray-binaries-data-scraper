51c8076d0fe992fe9c9a1bed0f68e3d95ba2f4e8
alicja-brzezowska <brzezowskaalicja0@gmail.com>
2025-08-22 14:36:26 +0200

    change save_csv function

--- scripts/functions.py
+++ scripts/functions.py
@@ -12,15 +12,23 @@ def read_csv(file_name):
         header = next(csv_reader)
         return list(csv_reader)
 
-
 def save_results_to_csv(rows, output_file, header):
-    "Save data in a csv file"
+    """Save rows to CSV. Works for both lists and dictonaries."""
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
-    with open(output_file, "w", newline="") as f:
-        w = csv.DictWriter(f, fieldnames=header)
-        w.writeheader()
-        for r in rows:
-            w.writerow({k: ("" if r.get(k) is None else r.get(k)) for k in header})
+    rows = list(rows or [])
+
+    with open(output_file, "w", newline="", encoding="utf-8") as f:
+        # If first row is a dict, use DictWriter
+        if rows and isinstance(rows[0], dict):
+            w = csv.DictWriter(f, fieldnames=header)
+            w.writeheader()
+            for r in rows:
+                w.writerow({k: ("" if r.get(k) is None else r.get(k)) for k in header})
+        else:
+            # Accomodate for lists
+            w = csv.writer(f)
+            w.writerow(header)
+            w.writerows(rows)
 
 
 def rect_to_radec(lmin, lmax, bmin, bmax, n=50):
